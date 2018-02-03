ad989cef57896d3c6cf31e09d6b494ac4b89a210
Jorge Ramirez <Jorge.X.Ramirez.-ND@disney.com>
2018-02-03 01:58:55 -0500

    Adds logging and makes the outputDirectoryPath parameter an option, in favor of a temporary directory

--- Package.swift
+++ Package.swift
@@ -5,14 +5,15 @@ import PackageDescription
 let package = Package(
     name: "class-generator",
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
-        .package(url: "https://github.com/kylef/Stencil.git", from: "0.10.1"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "4.0.3"),
-        .package(url: "https://github.com/Hearst-DD/ObjectMapper.git", from: "3.1.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMajor(from: "0.8.0")),
+        .package(url: "https://github.com/kylef/Stencil.git", .upToNextMajor(from: "0.10.1")),
+        .package(url: "https://github.com/jakeheis/SwiftCLI.git", .upToNextMajor(from: "4.0.3")),
+        .package(url: "https://github.com/Hearst-DD/ObjectMapper.git", .upToNextMajor(from: "3.1.0")),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMajor(from: "1.7.1"))
     ],
     targets: [
         .target(
             name: "class-generator",
-            dependencies: ["PathKit", "Stencil", "SwiftCLI", "ObjectMapper"])
+            dependencies: ["PathKit", "Stencil", "SwiftCLI", "ObjectMapper", "HeliumLogger"])
     ]
 )
--- Sources/class-generator/main.swift
+++ Sources/class-generator/main.swift
@@ -1,5 +1,16 @@
+import HeliumLogger
+import LoggerAPI
 import SwiftCLI
 
+// setup logging
+let logger = HeliumLogger(.info)
+logger.colored = true
+logger.format = "[(%date)] [(%type)] (%msg)"
+Log.logger = logger
+
+// setup the command line interface
 let cli = CLI(name: "class-generator", version: "0.0.1", description: "class-generator - Generate classes from JSON schemas")
 cli.commands = [GenerateCommand()]
+
+// run the command line interface
 let _ = cli.go()
