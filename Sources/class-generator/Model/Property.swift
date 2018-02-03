ef9249b29b9e3eee932c059b40ee0945d83b4f79
Jorge Ramirez <Jorge.X.Ramirez.-ND@disney.com>
2018-02-03 12:58:22 -0500

    Updates the class-generator JSON schema to reduce the number of properties needed

--- Sources/class-generator/Model/Property.swift
+++ Sources/class-generator/Model/Property.swift
@@ -5,40 +5,69 @@ internal class Property: ImmutableMappable {
 
     // MARK: - Public Properties
 
+    /// The name of the property.
+    /// Example: "user" or "firstName"
     let name: String
-    let type: Type
-    let isCollection: Bool
-    let isRequired: Bool
+
+    /// The property's data type, including if it is a collection and if it's optional.
+    /// Example: "String", "String?", "[String]" or "[String]?"
+    let type: String
+
+    /// An optional description of the property.
     let description: String?
+
+    // Whether the property is a collection.
+    var isCollection: Bool {
+        return type.hasPrefix("[")
+    }
+
+    /// Whether the property is optional.
+    var isOptional: Bool {
+        return type.hasSuffix("?")
+    }
+
+    /// The raw data type of the property.
+    /// Example: If the type is "[String]?" or "[String]", the rawType would be "String"
+    var rawType: String {
+        var startIndex = type.startIndex
+        var endIndex = type.endIndex
+
+        if isCollection {
+            // acount for the square brackets at the start and end of the type declaration
+            startIndex = type.index(after: startIndex)
+            endIndex = type.index(before: endIndex)
+        }
+
+        if isOptional {
+            // acount for the question mark at the end of the type declaration
+            endIndex = type.index(endIndex, offsetBy: -1)
+        }
+
+        return String(type[startIndex..<endIndex])
+    }
 
     // MARK: - Private Enums
 
     fileprivate enum Keys: String {
         case name
         case type
-        case isCollection
-        case isRequired
         case description
+        case isCollection
+        case isOptional
+        case rawType
     }
 
     // MARK: - Initialization
 
-    init(name: String, type: Type, isCollection: Bool = false, isRequired: Bool = true, description: String? = nil) {
+    init(name: String, type: String, description: String? = nil) {
         self.name = name
         self.type = type
-        self.isCollection = isCollection
-        self.isRequired = isRequired
         self.description = description
     }
 
     required init(map: Map) throws {
         name = try map.value(Keys.name.rawValue)
-
-        let typeString: String = try map.value(Keys.type.rawValue)
-        type = Type(stringValue: typeString)
-
-        isCollection = (try? map.value(Keys.isCollection.rawValue)) ?? false
-        isRequired = (try? map.value(Keys.isRequired.rawValue)) ?? true
+        type = try map.value(Keys.type.rawValue)
         description = try? map.value(Keys.description.rawValue)
     }
 
@@ -46,13 +75,12 @@ internal class Property: ImmutableMappable {
 
     func mapping(map: Map) {
         name >>> map[Keys.name.rawValue]
-
-        let typeString = type.stringValue()
-        typeString >>> map[Keys.type.rawValue]
+        type >>> map[Keys.type.rawValue]
+        description >>> map[Keys.description.rawValue]
 
         isCollection >>> map[Keys.isCollection.rawValue]
-        isRequired >>> map[Keys.isRequired.rawValue]
-        description >>> map[Keys.description.rawValue]
+        isOptional >>> map[Keys.isOptional.rawValue]
+        rawType >>> map[Keys.rawType.rawValue]
     }
 
 }
