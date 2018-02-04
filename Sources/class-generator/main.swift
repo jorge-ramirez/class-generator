d866ab928b83ce58585f28fb4a5a35a940129f57
Jorge Ramirez <jorge.a.ramirez@mac.com>
2018-02-04 12:29:30 -0500

    Bumps version to 1.0.2

--- Sources/class-generator/main.swift
+++ Sources/class-generator/main.swift
@@ -9,7 +9,7 @@ logger.format = "[(%date)] [(%type)] (%msg)"
 Log.logger = logger
 
 // setup the command line interface
-let cli = CLI(name: "class-generator", version: "1.0.1", description: "class-generator - Generate classes from JSON schemas")
+let cli = CLI(name: "class-generator", version: "1.0.2", description: "class-generator - Generate classes from JSON schemas")
 cli.commands = [GenerateCommand()]
 
 // run the command line interface
