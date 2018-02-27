1ceb72c44082ab8e9b29c147c6739094d082093d
Jorge Ramirez <jorge.a.ramirez@mac.com>
2018-02-27 16:21:27 -0500

    Bumps the version to 1.0.7

--- Sources/class-generator/main.swift
+++ Sources/class-generator/main.swift
@@ -19,7 +19,7 @@ Log.logger = logger
 
 // setup the command line interface
 private let cli = CLI(name: "class-generator",
-                      version: "1.0.6",
+                      version: "1.0.7",
                       description: "class-generator - Generate classes from JSON schemas")
 
 // configure and run the command line interface
